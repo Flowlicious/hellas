58610f4ca3b3f11b87a86971c210fc1dea71124c
Florian Gruehn <Florian.gruehn@netzkern.de>
2019-07-12 12:59:13 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import * as serviceWorker from "./serviceWorker";
 import Hellas from "./Hellas";
 import * as microsoftTeams from "@microsoft/teams-js";
 
-ReactDOM.render(<Hellas />, document.getElementById("root"));
-
 try {
   microsoftTeams.initialize();
 } catch (error) {}
+
+ReactDOM.render(<Hellas />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
